8d388e270582e93d3efc26e34cdffa8cc8ba1316
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-13 22:58:47 -0300

    Create trafficlight_for_padestrians.ino

---
+++ Traffic Light/Complex_TrafficLight/trafficlight_for_padestrians.ino
@@ -0,0 +1,20 @@
+//Definindo variáveis
+#define led_green_pedestrians 9
+#define led_red_pedestrians 10
+#define led_green_cars 11
+#define led_yellow_cars 12
+#define led_red_cars 13
+
+
+void setup() {
+    //Configurando os pinos dos LEDS para modo saída.
+    pinMode(led_green_pedestrians, OUTPUT);
+    pinMode(led_red_pedestrians, OUTPUT);
+    pinMode(led_green_cars, OUTPUT);
+    pinMode(led_yellow_cars, OUTPUT);
+    pinMode(led_red_cars, OUTPUT)
+}
+
+void loop() {
+
+}
