c3336a8820caf7cee9fe4fde52c964d6e8bb77a0
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-13 22:13:30 -0300

    Create basic_trafficlight.ino

---
+++ Traffic Light/Basic_TrafficLight/basic_trafficlight.ino
@@ -0,0 +1,31 @@
+//Definindo as variáveis.
+#define led_green 11
+#define led_yellow 12
+#define led_red 13
+
+void setup() {
+  //Configurando os pinos dos LEDS como OUTPUT(Saída).
+  pinMode(led_green, OUTPUT);
+  pinMode(led_yellow, OUTPUT);
+  pinMode(led_red, OUTPUT);
+}
+
+void loop() {
+  //Sinal aberto: apenas LED verde.
+  digitalWrite(led_green, HIGH);
+  digitalWrite(led_yellow, LOW);
+  digitalWrite(led_red, LOW);
+  delay(3000);
+  
+  //Sinal fechado: apenas LED amarelo.
+  digitalWrite(led_green, LOW);
+  digitalWrite(led_yellow, HIGH);
+  digitalWrite(led_red, LOW);
+  delay(1000);
+
+  //Sinal fechado - continuidade: apenas LED vermelho.
+  digitalWrite(led_green, LOW);
+  digitalWrite(led_yellow, LOW);
+  digitalWrite(led_red, HIGH);
+  delay(5000);
+}
