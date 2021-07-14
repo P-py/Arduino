1920519a6a410b101fe8b5ed3f5248d82799e67d
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-14 18:21:38 -0300

    Basic buzzer

---
+++ 4 - Buzzer/basic_buzzer/basic_buzzer.ino
@@ -0,0 +1,13 @@
+//Definindo variáveis
+int frequency = 2000;
+
+void setup() {
+  pinMode(10, OUTPUT); //Configurando o pino digital 10 como saída de dados.
+}
+
+void loop() {
+  tone(10, frequency); //Gera frequência de 2000hz para o pino digital 10, onde está localizado o buzzer.
+  delay(1000);
+  noTone(10); //Para de produzir frequência.
+  delay(1000);
+}
