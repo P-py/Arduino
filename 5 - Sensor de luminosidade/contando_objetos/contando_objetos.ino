3c0564fdeb8358fbc465923073530df3060cac45
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-16 13:27:22 -0300

    Counting objects script

---
+++ 5 - Sensor de luminosidade/contando_objetos/contando_objetos.ino
@@ -0,0 +1,48 @@
+// Variáveis de conexão
+const int pinoLDR = A0;
+const int pinoBuzzer = 9;
+const int pinoBotao = 8;
+
+// Variáveis
+int leituraLDR = 0;
+int contador = 0;
+int leituraBotao;
+int flag = 0;
+
+
+void setup() {
+  // Configurando os pinos e o serial monitor
+  pinMode(pinoLDR, INPUT);
+  pinMode(pinoBuzzer, OUTPUT);
+  pinMode(pinoBotao, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Coletando o valor de tensão no LDR
+  leituraLDR = analogRead(pinoLDR);
+
+  // Coletando o estado do botão
+  leituraBotao = digitalRead(pinoBotao);
+
+  if (leituraBotao == LOW) {
+    contador = 0;
+    Serial.println(contador);
+    delay(100);
+  }
+
+  if (leituraLDR <=512 && flag == 0) {
+    contador ++;
+    flag = 1;
+    tone(pinoBuzzer, 1000);
+    delay(100);
+    noTone(pinoBuzzer);
+  }
+
+  else if (leituraLDR > 512 && flag == 1) {
+    flag = 0;
+  }
+  delay(10);
+  Serial.print("Objetos contados: ");
+  Serial.println(contador);
+}
