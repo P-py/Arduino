59873bd3c31ec4a94cf8aaec6eff2aa505b58cd3
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-17 16:15:15 -0300

    Update projeto_pixel.ino

--- 7 - LED RGB/projeto_pixel/projeto_pixel.ino
+++ 7 - LED RGB/projeto_pixel/projeto_pixel.ino
@@ -1,50 +1,73 @@
 // Variáveis de conexão
-const int pinoRED = 11;
-const int pinoGREEN = 10;
-const int pinoBLUE = 9;
-const int pinoBttnR = 7;
-const int pinoBttnG = 6;
-const int pinoBttnB = 5;
+const int pinoLEDR = 11;
+const int pinoLEDG = 10;
+const int pinoLEDB = 9;
+const int pinoBotaoR = 7;
+const int pinoBotaoG = 6;
+const int pinoBotaoB = 5;
+const int pinoBotaoA = 4; // Botão de ajuste
+const int pinoPotenciometro = A0; // Potênciometro
 
 // Variáveis globais
-int BttnR;
-int BttnG;
-int BttnB;
+int leituraBotaoR;
+int leituraBotaoG;
+int leituraBotaoB;
+int leituraBotaoA;
+int leituraPotenciometro;
+int pwmR = 255;
+int pwmG = 255;
+int pwmB = 255;
 
 void setup() {
-  pinMode(pinoRED, OUTPUT);
-  pinMode(pinoGREEN, OUTPUT);
-  pinMode(pinoBLUE, OUTPUT);
-  pinMode(pinoBttnR, INPUT_PULLUP);
-  pinMode(pinoBttnG, INPUT_PULLUP);
-  pinMode(pinoBttnB, INPUT_PULLUP);
+  Serial.begin(9600); // Iniciando a Serial
+  // Configura os pinos
+  pinMode(pinoLEDR,OUTPUT);
+  pinMode(pinoLEDG,OUTPUT);
+  pinMode(pinoLEDB,OUTPUT);
+  pinMode(pinoBotaoR,INPUT_PULLUP);
+  pinMode(pinoBotaoG,INPUT_PULLUP);
+  pinMode(pinoBotaoB,INPUT_PULLUP);
+  pinMode(pinoBotaoA,INPUT_PULLUP);
+  pinMode(pinoPotenciometro,INPUT);
 }
 
 void loop() {
-  BttnR = digitalRead(pinoBttnR);
-  BttnG = digitalRead(pinoBttnG);
-  BttnB = digitalRead(pinoBttnB);
+  // Coleta o estado dos botões e do potenciômetro
+  leituraBotaoR = digitalRead(pinoBotaoR);
+  leituraBotaoG = digitalRead(pinoBotaoG);
+  leituraBotaoB = digitalRead(pinoBotaoB);
+  leituraBotaoA = digitalRead(pinoBotaoA);
+  leituraPotenciometro = analogRead(pinoPotenciometro);
 
-  if (BttnR == LOW) {
-    analogWrite(pinoRED, 255);
-  }
-  else {
-    analogWrite(pinoRED, 0);
-  }
-
-  if (BttnG == LOW) {
-    analogWrite(pinoGREEN, 255);
-  }
-  else {
-    analogWrite(pinoGREEN, 0);
-  }
-
-  if (BttnB == LOW) {
-    analogWrite(pinoBLUE, 255);
-  }
-  else {
-    analogWrite(pinoBLUE, 0);
+  /* Nas condicionais abaixo ele verifica se o botão de cada cor está ativo, se estiver irá verificar se 
+  o botão de ajuste está ativo e regular a cor, caso esteja, caso o botão da cor não estiver ativo simplesmente
+  não ativará o LED correspondente. */
+  if(leituraBotaoR == LOW) {
+    if (leituraBotaoA == LOW) {
+      pwmR = map(leituraPotenciometro, 0, 1023, 0, 255);
+    }
+    analogWrite(pinoLEDR, pwmR);
+  }else {
+    analogWrite(pinoLEDR, 0);
   }
   
+  if(leituraBotaoG == LOW) {
+    if (leituraBotaoA == LOW) {
+      pwmG = map(leituraPotenciometro, 0, 1023, 0, 255);
+    }
+    analogWrite(pinoLEDG, pwmG);
+  }else {
+    analogWrite(pinoLEDG, 0);
+  }
+  
+  if(leituraBotaoB == LOW) {
+    if (leituraBotaoA == LOW) {
+      pwmB = map(leituraPotenciometro, 0, 1023, 0, 255);
+    }
+    analogWrite(pinoLEDB, pwmB);
+  }else {
+    analogWrite(pinoLEDB, 0);
+  }
+  Serial.println(leituraPotenciometro);
   delay(100);
 }
