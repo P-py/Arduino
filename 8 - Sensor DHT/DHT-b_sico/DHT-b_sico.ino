0cdde036460033a6a6b3c7e224cb8f0ca358ebfc
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-17 17:08:54 -0300

    Optimizing the code

--- 8 - Sensor DHT/DHT-b_sico/DHT-b_sico.ino
+++ 8 - Sensor DHT/DHT-b_sico/DHT-b_sico.ino
@@ -10,31 +10,31 @@ float umidade; // Armazenará o valor de umidade
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
+  dht.begin(); // Inicializa as funções do sensor
 }
 
 void loop() {
-  delay(2000);
+  delay(2000); // Aguarda 2 segundos (necessário de acordo com a documentação do sensor)
 
-  temperatura = dht.readTemperature();
+  // A leitura pode levar até 250ms
+  // O atraso pode levar até 2000ms - 2s
+  temperatura = dht.readTemperature(); // Coleta a temperatura em Celsius
   umidade = dht.readHumidity();
-  if (isnan(umidade) || isnan(temperatura)) {
-    Serial.println("Falha na leitura do Sensor DHT!");
+
+  // Verifica se houve alguma falha na conexão/leitura do sensor.
+  // A função isnan verifica se o valor nos parêntesis é número ou não.
+  if (isnan(umidade) or isnan(temperatura)) {
+    Serial.println("Falha na leitura ou conexão do Sensor DHT!");
   }
   
-  else { // Se não
-    // Imprime o valor de temperatura  
+  else {
+    // Imprime os valores
     Serial.print("Temperatura: ");
     Serial.print(temperatura);
-    Serial.print(" *C ");
+    Serial.print(" ºC \t")
     
-    Serial.print("\t"); // tabulação
-  
-    // Imprime o valor de umidade
     Serial.print("Umidade: ");
     Serial.print(umidade);
-    Serial.print(" %\t"); 
-    
-    Serial.println(); // nova linha
+    Serial.println(" % \t"); 
   }
 }
