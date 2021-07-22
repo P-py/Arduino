2d74756e90a7d34716b81a761318f89c33bb4c44
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-22 04:04:36 -0300

    Create display_com_CI4511-INT.ino

---
+++ 9 - Display de 7 seg/display_com_CI4511-INT/display_com_CI4511-INT.ino
@@ -0,0 +1,96 @@
+// Variáveis de conexão
+const int pinoA = 12;
+const int pinoB = 9;
+const int pinoC = 10;
+const int pinoD = 11;
+
+// Variáveis globais
+int leitura_serial;
+
+void setup() {
+  // Configurando os pinos
+  pinMode(pinoA, OUTPUT);
+  pinMode(pinoB, OUTPUT);
+  pinMode(pinoC, OUTPUT);
+  pinMode(pinoD, OUTPUT);
+
+  // Iniciando e configurando a serial
+  Serial.begin(9600);
+  Serial.println("Digite um número de 0 a 9");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    leitura_serial = Serial.read();
+    Serial.println(leitura_serial);
+    leitura_serial = leitura_serial - 48;
+    if(leitura_serial == 0){
+          digitalWrite(pinoA, LOW);
+          digitalWrite(pinoB, LOW);
+          digitalWrite(pinoC, LOW);
+          digitalWrite(pinoD, LOW);
+    }
+    if(leitura_serial == 1){
+        digitalWrite(pinoA, HIGH);
+        digitalWrite(pinoB, LOW);
+        digitalWrite(pinoC, LOW);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 2){
+        digitalWrite(pinoA, LOW);
+        digitalWrite(pinoB, HIGH);
+        digitalWrite(pinoC, LOW);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 3){
+        digitalWrite(pinoA, HIGH);
+        digitalWrite(pinoB, HIGH);
+        digitalWrite(pinoC, LOW);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 4){
+        digitalWrite(pinoA, LOW);
+        digitalWrite(pinoB, LOW);
+        digitalWrite(pinoC, HIGH);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 5){
+        digitalWrite(pinoA, HIGH);
+        digitalWrite(pinoB, LOW);
+        digitalWrite(pinoC, HIGH);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 6){
+        digitalWrite(pinoA, LOW);
+        digitalWrite(pinoB, HIGH);
+        digitalWrite(pinoC, HIGH);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 7){
+        digitalWrite(pinoA, HIGH);
+        digitalWrite(pinoB, HIGH);
+        digitalWrite(pinoC, HIGH);
+        digitalWrite(pinoD, LOW);
+    }
+      
+    if(leitura_serial == 8){
+        digitalWrite(pinoA, LOW);
+        digitalWrite(pinoB, LOW);
+        digitalWrite(pinoC, LOW);
+        digitalWrite(pinoD, HIGH);
+    }
+      
+    if(leitura_serial == 9){
+        digitalWrite(pinoA, HIGH);
+        digitalWrite(pinoB, LOW);
+        digitalWrite(pinoC, LOW);
+        digitalWrite(pinoD, HIGH);
+    }
+  }
+}
