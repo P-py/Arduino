0f93b0275cad72837a881cd7ef81592d8e1cd109
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-22 03:26:31 -0300

    Adding new script at section 9

--- 9 - Display de 7 seg/display_com_CI4511/display_com_CI4511.ino
+++ 9 - Display de 7 seg/display_com_CI4511/display_com_CI4511.ino
@@ -1,3 +1,5 @@
+// A função principal do circuito integrado CI4511 é traduzir/converter BCD para o display de 7 seg.
+
 // Variáveis de conexão
 const int pinoA = 12;
 const int pinoB = 9;
@@ -13,9 +15,73 @@ void setup() {
 }
 
 void loop() {
+  // Liga os bits necessários para formar 0 = 0000
   digitalWrite(pinoA, LOW);
   digitalWrite(pinoB, LOW);
   digitalWrite(pinoC, LOW);
   digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 1 = 0001
+  digitalWrite(pinoA, HIGH);
+  digitalWrite(pinoB, LOW);
+  digitalWrite(pinoC, LOW);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 2 = 0010
+  digitalWrite(pinoA, LOW);
+  digitalWrite(pinoB, HIGH);
+  digitalWrite(pinoC, LOW);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 3 = 0011
+  digitalWrite(pinoA, HIGH);
+  digitalWrite(pinoB, HIGH);
+  digitalWrite(pinoC, LOW);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 4 = 0100
+  digitalWrite(pinoA, LOW);
+  digitalWrite(pinoB, LOW);
+  digitalWrite(pinoC, HIGH);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 5 = 0101
+  digitalWrite(pinoA, HIGH);
+  digitalWrite(pinoB, LOW);
+  digitalWrite(pinoC, HIGH);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 6 = 0110
+  digitalWrite(pinoA, LOW);
+  digitalWrite(pinoB, HIGH);
+  digitalWrite(pinoC, HIGH);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 7 = 0111
+  digitalWrite(pinoA, HIGH);
+  digitalWrite(pinoB, HIGH);
+  digitalWrite(pinoC, HIGH);
+  digitalWrite(pinoD, LOW);
+  delay(1000);
+
+  // Liga os bits necessários para formar 8 = 1000
+  digitalWrite(pinoA, LOW);
+  digitalWrite(pinoB, LOW);
+  digitalWrite(pinoC, LOW);
+  digitalWrite(pinoD, HIGH);
+  delay(1000);
+
+  // Liga os bits necessários para formar 9 = 1001
+  digitalWrite(pinoA, HIGH);
+  digitalWrite(pinoB, LOW);
+  digitalWrite(pinoC, LOW);
+  digitalWrite(pinoD, HIGH);
   delay(1000);
 }
