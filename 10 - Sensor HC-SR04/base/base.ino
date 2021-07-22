b58ceed8b19b165d7d6f48cb5cb8364173351287
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-22 04:31:13 -0300

    Create base.ino

---
+++ 10 - Sensor HC-SR04/base/base.ino
@@ -0,0 +1,35 @@
+// Variáveis de conexão
+const int trig = 3;
+const int echo = 2;
+const int distancia_obstaculo = 20;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  int distancia = sensor_morcego(trig, echo);
+  if (distancia <= distancia_obstaculo) {
+    Serial.print("Com obstáculo: ");
+    Serial.print(distancia);
+    Serial.println("cm");
+  }
+  else {
+    Serial.print("Sem obstáculo: ");
+    Serial.print(distancia);
+    Serial.println("cm");
+  }
+  delay(500);
+}
+
+int sensor_morcego(int pinotrig, int pinoecho) {
+  digitalWrite(pinotrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinotrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinotrig, LOW);
+
+  return pulseIn(pinoecho, HIGH)/58;
+}
