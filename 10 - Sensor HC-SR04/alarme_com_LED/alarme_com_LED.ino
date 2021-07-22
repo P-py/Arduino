da7c1a95d479f86b5b2e06451120742608c7da42
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-22 17:51:48 -0300

    Create alarme_com_LED.ino

---
+++ 10 - Sensor HC-SR04/alarme_com_LED/alarme_com_LED.ino
@@ -0,0 +1,56 @@
+// Variáveis de conexão
+const int trig = 3;
+const int echo = 2;
+const int pinoBuzzer = 7;
+const int pinoLED = 8;
+
+// Variáveis globais
+const int distancia_obstaculo = 50;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(pinoBuzzer, OUTPUT);
+}
+
+void loop() {
+  int distancia = sensor_morcego(trig, echo);
+  if (distancia <= distancia_obstaculo) {
+    Serial.print("Com obstáculo: ");
+    Serial.print(distancia);
+    Serial.println("cm");
+    tone(pinoBuzzer, 2500);
+    digitalWrite(pinoLED, HIGH);
+    delay(100);
+    noTone(pinoBuzzer);
+    digitalWrite(pinoLED, LOW);
+    delay(100);
+    tone(pinoBuzzer, 2500);
+    digitalWrite(pinoLED, HIGH);
+    delay(100);
+    noTone(pinoBuzzer);
+    digitalWrite(pinoLED, LOW);
+    delay(100);
+    tone(pinoBuzzer, 2500);
+    digitalWrite(pinoLED, HIGH);
+  }
+  else {
+    Serial.print("Sem obstáculo: ");
+    Serial.print(distancia);
+    Serial.println("cm");
+    noTone(pinoBuzzer);
+    digitalWrite(pinoLED, LOW);
+  }
+  delay(100);
+}
+
+int sensor_morcego(int pinotrig, int pinoecho) {
+  digitalWrite(pinotrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinotrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinotrig, LOW);
+
+  return pulseIn(pinoecho, HIGH)/58;
+}
