7c8916fdc65f410cfb3d71df7ab9e902f552831c
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-22 03:15:53 -0300

    Create display_com_CI4511.ino

---
+++ 9 - Display de 7 seg/display_com_CI4511/display_com_CI4511.ino
@@ -0,0 +1,15 @@
+// Variáveis de conexão
+const int pinoA = 12;
+const int pinoB = 9;
+const int pinoC = 10;
+const int pinoD = 11;
+
+void setup() {
+  // Configuração dos pinos
+  pinMode(pino_a, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
