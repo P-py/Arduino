33ece1c179e41a641ec8d1ebb6038cabb77ae947
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-07-22 17:47:02 -0300

    HCSR04 + Buzzer

--- 10 - Sensor HC-SR04/base_com_alarme/base_com_alarme.ino
+++ 10 - Sensor HC-SR04/base_com_alarme/base_com_alarme.ino
@@ -1,12 +1,15 @@
 // Variáveis de conexão
 const int trig = 3;
 const int echo = 2;
-const int distancia_obstaculo = 20;
+const int pinoBuzzer = 7;
+// Variáveis globais
+const int distancia_obstaculo = 50;
 
 void setup() {
   Serial.begin(9600);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+  pinMode(pinoBuzzer, OUTPUT);
 }
 
 void loop() {
@@ -15,13 +18,15 @@ void loop() {
     Serial.print("Com obstáculo: ");
     Serial.print(distancia);
     Serial.println("cm");
+    tone(pinoBuzzer, 2500);
   }
   else {
     Serial.print("Sem obstáculo: ");
     Serial.print(distancia);
     Serial.println("cm");
+    noTone(pinoBuzzer);
   }
-  delay(200);
+  delay(100);
 }
 
 int sensor_morcego(int pinotrig, int pinoecho) {
